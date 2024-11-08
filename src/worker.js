3be543a0961ba366d9c47892d02a750255333d02
yansuogaming <yansuogaming@gmail.com>
2024-11-08 09:23:32 +0700

    Update

--- src/worker.js
+++ src/worker.js
@@ -251,7 +251,7 @@ const homepage = `<!DOCTYPE html>
     </div>
    </body>
   <script src="${uiConfig.jsdelivr_cdn_src}@${uiConfig.version}/assets/homepage.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </html>`
 
 const login_html = `<html>
